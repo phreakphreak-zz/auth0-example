0a571329ace5e85b2ee7717b82d2884b49c094e1
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-05-20 14:57:23 -0400

    implement login logout and profile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { LoginButton } from "./Login";
+import { Profile } from "./Profile";
+import { LogoutButton } from "./Logout";
+import logo from "./logo.svg";
+import "./App.css";
+import { useAuth0 } from "@auth0/auth0-react";
+import React, { Fragment } from "react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {isAuthenticated ? (
+          <Fragment>
+            <Profile />
+            <LogoutButton />
+          </Fragment>
+        ) : (
+          <LoginButton />
+        )}
       </header>
     </div>
   );
